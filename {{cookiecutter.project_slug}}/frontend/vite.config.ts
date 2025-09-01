0bce451546b0c54b5945ea6a846090a8c9449ce8
Rishab Manocha <rishab.manocha@outlook.com>
2025-09-01 11:26:22 -0700

    Fix indendation

--- {{cookiecutter.project_slug}}/frontend/vite.config.ts
+++ {{cookiecutter.project_slug}}/frontend/vite.config.ts
@@ -6,7 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [
+  plugins: [
     react(),
     tailwindcss(),
     VitePWA({
